f9a32c292f595f9d2d7c0e5698ce0d23e5268765
Saieesh03 <saie3shrawool@gmail.com>
2026-02-22 19:57:40 +0530

    features.py

--- features.py
+++ features.py
@@ -1,13 +1,10 @@
-# feature_pipeline.py
 from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA
 
 def scale_features(X):
     scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
-    return X_scaled
+    return scaler.fit_transform(X)
 
 def apply_pca(X, n_components=2):
     pca = PCA(n_components=n_components)
-    X_pca = pca.fit_transform(X)
-    return X_pca
+    return pca.fit_transform(X)
