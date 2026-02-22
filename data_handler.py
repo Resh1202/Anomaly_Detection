8f6ed802ad41ad17a7f2fc13d52001679ca48cdd
Saieesh03 <saie3shrawool@gmail.com>
2026-02-22 19:57:22 +0530

    data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def load_uploaded_file(uploaded_file):
-    df = pd.read_csv(uploaded_file)
-    return df
+    return pd.read_csv(uploaded_file)
 
 def basic_validation(df):
     if df.isnull().sum().sum() > 0:
