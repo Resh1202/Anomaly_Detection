e3e83f4cb880f1795b95657d4e12dea5c6a570f6
Saieesh03 <saie3shrawool@gmail.com>
2026-02-22 19:51:10 +0530

    evaluation.py

---
+++ evaluation.py
@@ -0,0 +1,17 @@
+# evaluation_metrics.py
+from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
+import matplotlib.pyplot as plt
+
+def show_classification_metrics(y_true, y_pred):
+    report = classification_report(y_true, y_pred)
+    cm = confusion_matrix(y_true, y_pred)
+    return report, cm
+
+def plot_roc_curve(y_true, scores):
+    fpr, tpr, _ = roc_curve(y_true, -scores)
+    roc_auc = auc(fpr, tpr)
+    
+    plt.figure()
+    plt.plot(fpr, tpr, label=f"AUC = {roc_auc:.2f}")
+    plt.legend()
+    return plt
