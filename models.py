bbe97c29961c0c7e6ffd0e6dfeef46291cdbb4f3
Saieesh03 <saie3shrawool@gmail.com>
2026-02-22 12:26:13 +0530

    models.py

--- models.py
+++ models.py
@@ -1,21 +1,9 @@
 from sklearn.ensemble import IsolationForest
-from sklearn.neighbors import LocalOutlierFactor
-from sklearn.svm import OneClassSVM
 
 def detect_anomalies(model_name, data, contamination=0.05):
 
     if model_name == "Isolation Forest":
         model = IsolationForest(contamination=contamination, random_state=42)
-        labels = model.fit_predict(data)
-        scores = model.decision_function(data)
-
-    elif model_name == "Local Outlier Factor":
-        model = LocalOutlierFactor(contamination=contamination)
-        labels = model.fit_predict(data)
-        scores = -model.negative_outlier_factor_
-
-    elif model_name == "One-Class SVM":
-        model = OneClassSVM(nu=contamination)
         labels = model.fit_predict(data)
         scores = model.decision_function(data)
 
